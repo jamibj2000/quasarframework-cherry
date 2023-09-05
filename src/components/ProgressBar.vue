41ec1d268b0c56cc9bb529e4fdd7046da1f85afb
Jamiel <silentrakan@gmail.com>
2023-09-05 06:31:37 -0500

    N

---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="q-pa-md bg-grey-9 text-white">
+    <q-linear-progress dark stripe rounded size="20px" :value="progress2" color="red" class="q-mt-sm" />
+  </div>
+</template>
+
+
+<script setup>
+  import { ref } from 'vue'
+  const progress2 = ref(0)
+</script>
+
+<template>
+  <button @click="count++">Count is: {{ count }}</button>
+</template>
+
+<style scoped>
+  button {
+    font-weight: bold;
+  }
+</style>
